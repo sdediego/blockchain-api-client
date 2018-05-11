97c1965e995ac4a17f68fb2704ecc892ae4d6798
Sergio de Diego <sergio.dediego@outlook.com>
2018-05-11 23:07:46 +0200

    Update init

---
+++ blockchainAPI/resources/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+__author__ = 'Sergio de Diego'
+__url__ = 'github.com/sdediego'
+__version__ = '1.0'
+__date__ = 'March 2018'
+
+__all__ = ['BlockchainAPIChart']
