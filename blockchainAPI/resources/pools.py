57e4937efd7df14d81fe2b3fd421dcbf29f53900
Sergio de Diego <sergio.dediego@outlook.com>
2018-05-14 21:13:31 +0200

    Add docs to pools resource class

--- blockchainAPI/resources/pools.py
+++ blockchainAPI/resources/pools.py
@@ -5,17 +5,36 @@ import json
 
 
 class BlockchainAPIPool(object):
+    """
+    Get pools data behind Blockchain API.
+    """
 
     def __init__(self, pools, *args, **kwargs):
+        """
+        Initialize Blockchain API for pools.
+
+        :param json pools: json object with pools data.
+        """
         self._pools = pools
         for key, value in self._pools.items():
             var_name = key.replace(" ", "").replace(".", "").lower()
             setattr(self, '_{}'.format(var_name), {'pool': key, 'hashrate': value})
 
     def __str__(self):
+        """
+        Represent class via params string.
+
+        :return str: class representarion.
+        """
         return '<{} - Bitcoin pools>'.format(self.__class__.__name__)
 
     def get_info(self, pool):
+        """
+        Get pool hash rate distribution information.
+
+        :param str pool: pool name.
+        :return dict: pool hash rate contribution.
+        """
         pool_name = list(filter(lambda name: name == pool, self._pools.keys()))
         if len(pool_name) > 0:
             var_name = pool_name[0].replace(" ", "").replace(".", "").lower()
@@ -25,8 +44,18 @@ class BlockchainAPIPool(object):
 
     @property
     def pools(self):
+        """
+        Get mining pools list from Blockchain API.
+
+        :return list: Bitcoin mining pools list.
+        """
         return list(self._pools.keys())
 
     @property
     def response(self):
+        """
+        Get response from Blockchain API.
+
+        :return str: Blockchain API response.
+        """
         return json.dumps(self._pools)
