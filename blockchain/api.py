29b91f4ed4c7413d04353650934a6f4102e1d65d
Sergio de Diego <sergio.dediego@outlook.com>
2018-05-16 22:10:38 +0200

    Add str method to blockchain api client class

--- blockchain/api.py
+++ blockchain/api.py
@@ -9,3 +9,12 @@ class BlockchainAPIClient(object):
         self._api_url = api_url
         self._api_key = api_key
         self._request_params = {}
+
+    def __str__(self):
+        params = {
+            'classname': self.__class__.__name__,
+            'data': self._api_data
+        }
+        for key, value in self._request_params.items():
+            params.update({'{}'.format(key): value})
+        return str(params)
