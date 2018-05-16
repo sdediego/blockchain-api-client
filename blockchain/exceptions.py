5b89c7bfe579b6c3162def0fe084dc3dc513d065
Sergio de Diego <sergio.dediego@outlook.com>
2018-05-16 22:13:19 +0200

    Create base error exception class

---
+++ blockchain/exceptions.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+class BaseError(Exception):
+    """
+    Handle generic exception.
+    """
+
+    def __init__(self, msg, code=None):
+        if code is not None:
+            self.code = code
+            msg += ': {exception}'.format(exception=self.code)
+        self.msg = msg
+        super(BaseError, self).__init__(self.msg)
