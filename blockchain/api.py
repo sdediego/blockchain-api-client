5e046b43a44673ff506b5bdbddb42a946353c26f
Sergio de Diego <sergio.dediego@outlook.com>
2018-05-16 22:20:45 +0200

    Add config class method to blockchain api client class

--- blockchain/api.py
+++ blockchain/api.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+import configparser
+import os
+
+from .exceptions import BlockchainAPIClientError
+
 
 class BlockchainAPIClient(object):
 
@@ -18,3 +23,16 @@ class BlockchainAPIClient(object):
         for key, value in self._request_params.items():
             params.update({'{}'.format(key): value})
         return str(params)
+
+    @classmethod
+    def config(cls, data=None, filename='blockchain.cfg', section='api'):
+        parser = configparser.ConfigParser()
+        parser.read(filename)
+        if parser.has_section(section):
+            base_url = parser.get(section, 'base_url')
+            data_url = parser.get(section, data)
+            api_key = os.getenv('API_KEY')
+            return cls(data, base_url + data_url, api_key)
+        else:
+            msg = 'Section {} not found in {} file'.format(section, filename)
+            raise BlockchainAPIClientError(msg)
