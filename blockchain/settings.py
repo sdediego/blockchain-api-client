7b44c80547438a2832285dedaf73cceba1cab92a
Sergio de Diego <sergio.dediego@outlook.com>
2018-05-29 22:12:49 +0200

    Add reblockchain api resources

---
+++ blockchain/settings.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+# Settings for Blockchain API project
+
+
+# Configure Blockchain API resources
+RESOURCES = {
+    'charts': 'BlockchainAPIChart',
+    'stats': 'BlockchainAPIStatistics',
+    'pools': 'BlockchainAPIPool',
+}
