40775bcb74ac87815437709e485bd22c605f543f
Sergio de Diego <sergio.dediego@outlook.com>
2018-05-30 23:38:57 +0200

    Update init

--- blockchain/__init__.py
+++ blockchain/__init__.py
@@ -5,7 +5,7 @@
 __author__ = 'Sergio de Diego'
 __url__ = 'github.com/sdediego'
 __version__ = '1.0'
-__date__ = 'February 2018'
+__date__ = 'March 2018'
 
 __all__ = [
     'BlockchainAPIClient', 'BlockchainAPIHttpRequest', 'BlockchainAPIHttpResponse',
