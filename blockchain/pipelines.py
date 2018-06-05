a4faf4ba91e67436693a629668f7f5696b8ce7dc
Sergio de Diego <sergio.dediego@outlook.com>
2018-06-05 23:30:24 +0200

    Add config classmethod to json file writer pipeline class

--- blockchain/pipelines.py
+++ blockchain/pipelines.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+import os
+
+from dotenv import load_dotenv
+from logging.config import fileConfig
+from os.path import dirname, join
+
+# Load .env file
+dotenv_path = join(dirname(dirname(__file__)), '.env')
+load_dotenv(dotenv_path)
+
 
 class JSONFileWriterPipeline(object):
     """
@@ -27,3 +37,17 @@ class JSONFileWriterPipeline(object):
             'filename': basename(self._file),
         }
         return str(params)
+
+    @classmethod
+    def config(cls):
+        """
+        Get JSON file parameters.
+
+        :return cls: JSON file writer class.
+        """
+        filepath = os.getenv('JSON_FILE_PATH')
+        if filepath is not None:
+            return cls(filepath)
+        else:
+            msg = 'Incorrect JSON file configuration: {}'.format(filepath)
+            raise ValueError(msg)
