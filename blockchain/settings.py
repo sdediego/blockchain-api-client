cff3ab7160c4cc50e9f347e6abcba82167a57472
Sergio de Diego <sergio.dediego@outlook.com>
2018-06-28 22:14:09 +0200

    Add chart settings

--- blockchain/settings.py
+++ blockchain/settings.py
@@ -10,3 +10,35 @@ RESOURCES = {
     'stats': 'BlockchainAPIStatistics',
     'pools': 'BlockchainAPIPool',
 }
+
+# Blockchain Charts
+CHARTS = [
+    # Bitcoin stats
+    'total-bitcoins',
+    'market-price',
+    'market-cap',
+    'trade-volume',
+    # Blockchain stats
+    'blocks-size',
+    'avg-block-size',
+    'n-transactions-per-block',
+    'median-confirmation-time',
+    # Mining stats
+    'hash-rate',
+    'difficulty',
+    'miners-revenue',
+    'transaction-fees',
+    'transaction-fees-usd',
+    'cost-per-transaction-percent',
+    'cost-per-transaction',
+    # Network activity
+    'n-unique-addresses',
+    'n-transactions',
+    'n-transactions-total',
+    'transactions-per-second',
+    'mempool-size',
+    'utxo-count',
+    'output-volume',
+    'estimated-transaction-volume',
+    'estimated-transaction-volume-usd',
+]
