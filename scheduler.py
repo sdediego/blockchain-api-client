f7c3805d0ae894a3ad610a11ad28b03f9f0eda10
Sergio de Diego <sergio.dediego@outlook.com>
2018-07-02 22:06:56 +0200

    Implement pools scheduled job

--- scheduler.py
+++ scheduler.py
@@ -7,6 +7,9 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from logging.config import fileConfig
 from os.path import dirname, join
 
+from blockchain.api import BlockchainAPIClient
+from blockchain.pipelines import MongoDBPipeline
+
 # Custom logger
 fileConfig(join(dirname(dirname(__file__)), 'logging.cfg'))
 logger = logging.getLogger(__name__)
@@ -22,9 +25,19 @@ def charts_job():
 def stats_job():
     pass
 
-@scheduler.scheduled_job()
+@scheduler.scheduled_job(id='pools', trigger='cron', day_of_week='mon-sun', hour=0)
 def pools_job():
-    pass
+    logger.info('Fetching bitcoin mining pools data.')
+
+    api = BlockchainAPIClient('pools')
+    result = api.call(timespan='5days')
+
+    logger.info('Persisting fetched data in MongoDB: %s', result)
+    mongo = MongoDBPipeline.config()
+    mongo.open_connection()
+    mongo.persist_data(result.response)
+    mongo.close_connection()
+    logger.info('Data successfully persisted.')
 
 # Start queueing jobs
 scheduler.start()
