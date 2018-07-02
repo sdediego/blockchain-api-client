8e24a8300e84cb48ace086b2f3a38e4fe920853c
Sergio de Diego <sergio.dediego@outlook.com>
2018-07-02 20:45:15 +0200

    Initialize blocking scheduler

---
+++ scheduler.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import logging
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+from logging.config import fileConfig
+from os.path import dirname, join
+
+# Custom logger
+fileConfig(join(dirname(dirname(__file__)), 'logging.cfg'))
+logger = logging.getLogger(__name__)
+
+scheduler = BlockingScheduler()
+
+scheduler.start()
