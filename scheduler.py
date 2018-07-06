39f800b8f10209a8fcccd2f21ef5b3c7e472b968
Sergio de Diego <sergio.dediego@outlook.com>
2018-07-06 23:40:15 +0200

    Update scheduled jobs

--- scheduler.py
+++ scheduler.py
@@ -8,6 +8,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from logging.config import fileConfig
 from os.path import dirname, join
 
+from blockchain import settings
 from blockchain.api import BlockchainAPIClient
 from blockchain.pipelines import MongoDBPipeline
 
@@ -19,6 +20,11 @@ scheduler = BlockingScheduler()
 
 
 def fetch_and_persist_data(data, *args, **kwargs):
+    """
+    Get and save data from Blockchain API.
+
+    :param str data: type of data (charts, stats, pools).
+    """
     # Retrieve blockchain data
     api = BlockchainAPIClient(data)
     result = api.call(**kwargs)
@@ -33,17 +39,26 @@ def fetch_and_persist_data(data, *args, **kwargs):
 
 @scheduler.scheduled_job()
 def charts_job():
+    """
+    Get and save blockchain charts data from Blockchain API.
+    """
     for chart in settings.CHARTS:
         logger.info('Fetching %s chart data.', chart)
         fetch_and_persist_data('charts', chart=chart, timespan='all')
 
 @scheduler.scheduled_job(id='stats', trigger='cron', day_of_week='mon-sun', hour=0)
 def stats_job():
+    """
+    Get and save blockchain stats data from Blockchain API.
+    """
     logger.info('Fetching blockchain statistical data.')
     fetch_and_persist_data('stats')
 
 @scheduler.scheduled_job(id='pools', trigger='cron', day_of_week='mon-sun', hour=0)
 def pools_job():
+    """
+    Get and save blockchain pools data from Blockchain API.
+    """
     logger.info('Fetching bitcoin mining pools data.')
     fetch_and_persist_data('pools', timespan='5days')
 
